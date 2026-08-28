7f46d64f8ac2469ed937de21041fff487011cd02
Surabhi Yadav <surabhiiyadav3@gmail.com>
2026-08-28 14:30:47 +0530

    Time: 11 ms (14.33%), Space: 8.7 MB (14.27%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int ans = 0;
+
+        for (long long i = 1; i <= x; i++) {   
+            if (i * i <= x) {
+                ans = i;
+            } else {
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
